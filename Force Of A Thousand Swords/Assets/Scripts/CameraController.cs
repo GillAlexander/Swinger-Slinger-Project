d3865219059fb5383a89bacc0738ed100bd26106
Jonathan A.O <jonte962010@hotmail.com>
2020-01-31 15:46:41 +0100

    Added unity project

---
+++ Force Of A Thousand Swords/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jonathan
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Camera mainCamera;
+        void Start()
+        {
+            mainCamera = FindObjectOfType<Camera>(); 
+        }
+        void Update()
+        {
+
+        }
+    }
+}
