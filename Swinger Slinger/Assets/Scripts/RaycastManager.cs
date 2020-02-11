694b79c38cf32852e21731f611cf1c83bc6e7809
Jonathan <jote962010@hotmail.com>
2020-02-11 18:54:50 +0100

    SpawnRope

--- Swinger Slinger/Assets/Scripts/RaycastManager.cs
+++ Swinger Slinger/Assets/Scripts/RaycastManager.cs
@@ -9,32 +9,29 @@ namespace Jonathan
         private Camera mainCamera = default;
         private Ray ray = default;
         private RaycastHit raycastHit = default;
-        
+        private ButtonController controller = default;
         void Start()
         {
             mainCamera = GetComponent<Camera>();
+            controller = FindObjectOfType<ButtonController>();
         }
 
         void Update()
         {
-            if (Time.frameCount % 8)
+            if (Time.frameCount % 8 == 0) // Optimera mera
             {
-
-            }
-            UpdateRay();
-            if (Physics.Raycast(ray, out raycastHit))
-            {
-                Debug.Log(raycastHit.transform.GetComponent<IInteractable>());
+                if (controller.RightMouseButtonHeldDown)
+                {
+                    UpdateRay();
+                    if (Physics.Raycast(ray, out raycastHit))
+                    {
+                        Debug.Log(raycastHit.transform.GetComponent<IInteractable>());
+                    }
+                }
             }
             Debug.DrawRay(ray.origin, ray.direction * 30f, Color.blue);
         }
 
-        private void UpdateRay()
-        {
-            //if (true)
-            //{
-                ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            //}
-        }
+        private void UpdateRay() => ray = mainCamera.ScreenPointToRay(Input.mousePosition);
     }
 }
---
+++ Swinger Slinger/Assets/Scripts/SpawnRope.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRope : MonoBehaviour
+{
+    [SerializeField] GameObject ropePrefab = default;
+    [SerializeField] GameObject ropeParent = default;
+    [SerializeField] CharacterJoint ropeJoint = default;
+    
+    [SerializeField] [Range(1, 50)] private float ropeLength = default;
+    [SerializeField] private float ropePartLength = default;
+    [SerializeField] bool spawn = default;
+
+    private void Update()
+    {
+        if (spawn)
+        {
+            InstantiateRope();
+            spawn = false;
+        }
+    }
+    private void InstantiateRope()
+    {
+        var count = (int)(ropeLength / ropePartLength);
+
+        for (int x = 0; x < count; x++)
+        {
+            GameObject tmp;
+
+            tmp = Instantiate(ropePrefab, new Vector3(transform.position.x,
+                                                                           transform.position.y + ropePartLength * (x + 1),
+                                                                           transform.position.z),
+                                                                           Quaternion.identity, ropeParent.transform);
+            tmp.transform.eulerAngles = new Vector3(180, 0, 0);
+
+            tmp.name = ropeParent.transform.childCount.ToString();
+
+            if (x == 0)
+            {
+                Destroy(tmp.GetComponent<CharacterJoint>());
+            }
+            else
+            {
+                tmp.GetComponent<CharacterJoint>().connectedBody = ropeParent.transform.Find((ropeParent.transform.childCount - 1).ToString()).GetComponent<Rigidbody>();
+            }
+        }
+    }
+}
